8635b12457551e1aa8dd2f041f1eb5d6d8d4cef6
Faiz Khatri <faizkhatri00@gmail.com>
2025-01-24 10:59:58 +0530

    add @sanity/client package, remove tailwind-line-clamp plugin as it is already included in tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-import lineClamp from "@tailwindcss/line-clamp";
 
 export default {
   content: [
@@ -15,5 +14,5 @@ export default {
       },
     },
   },
-  plugins: [lineClamp],
+  plugins: [],
 } satisfies Config;
